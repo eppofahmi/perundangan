6b9241b0c12abb05b3eb6ed9816180f201806af2
UJANG FAHMI <eppofahmi@gmail.com>
2021-09-16 09:38:00 +0700

    add init

--- PreProcessingR/lib/helpers.R
+++ PreProcessingR/lib/helpers.R
@@ -1,4 +1,28 @@
-helper.function <- function()
-{
-  return(1)
+fn_uu = function(jenis, tahun){
+  perundangan = jenis
+  tahun = tahun
+  full_url = paste0("http://www.dpr.go.id/jdih/", perundangan, "/year/", tahun)
+  page = read_html(full_url)
+  
+  tabel = page %>% 
+    html_table(fill = TRUE)
+  tabel = tabel[[1]]
+  
+  # URL 
+  link = page %>%
+    html_nodes("div:nth-child(1) > a") %>% 
+    html_attr("href") %>% 
+    data_frame()
+  colnames(link) = "url"
+  
+  link = link %>% 
+    filter(str_detect(url, ".pdf")) %>% 
+    mutate(url = paste0("http://www.dpr.go.id", url))
+  
+  df = bind_cols(tabel, link)
+  df = df %>% 
+    select(-DOKUMEN)
+  colnames(df) = c("no", "judul", "url_pdf")
+  rm(tabel, page, link)
+  return(df)
 }
---
+++ PreProcessingR/munge/01-scraper_jdihdpr.R
@@ -0,0 +1,9 @@
+library(ProjectTemplate)
+setwd("PreProcessingR")
+load.project()
+
+# undang-undang ------------------------------------------------------------------------
+uu2020 = fn_uu(jenis = "uu", tahun = 2020)
+uu2019 = fn_uu(jenis = "uu", tahun = 2019)
+uu2018 = fn_uu(jenis = "uu", tahun = 2018)
+# undang-undang ------------------------------------------------------------------------
